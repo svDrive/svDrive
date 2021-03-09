c4ae41a57949319a7f448a6365d1680db7ddc447
ChrisShocker <FallenChris@live.com>
2021-03-09 15:05:24 -0800

    Demonstrate controller thumb-pad axis's are acknowledge by the app.
    Console.logs axis's input directions.
    Add more comments.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
+
+var controller = {};
+var buttons = [];
+var axis = [];
+
+//deadzone for axis, handler doesn't trip unless input > or < deadzone 
+var posDeadzone = 0.8;
+var negDeadzone = -0.8;
+
 const statusButton = document.querySelector("circle");
+
+//axis directions
+var lsL = 0;
+var lsR = 0;
+var lsU = 0;
+var lsD = 0;
+var rsL = 0;
+var rsR = 0;
+var rsU = 0;
+var rsD = 0;
+
 
 window.addEventListener("gamepadconnected", (e) => {
 	controller = e.gamepad;
@@ -25,12 +45,6 @@ window.addEventListener("gamepaddisconnected", (event) => {
 	statusButton.setAttribute("fill", "red");
 });
 
-var controller = {};
-var buttons = [];
-var axis = [];
-var posDeadzone = 0.8;
-var negDeadzone = -0.8;
-
 function updateHandler() {
 	buttons = [];
 	if (controller.buttons) {
@@ -44,18 +58,30 @@ function updateHandler() {
 
 	if (controller.axes) {
 		for (var i = 0; i < controller.axes.length; ++i) {
-			if (controller.axes[i] > posDeadzone || controller.axes[i] < negDeadzone) {
+			if (
+				controller.axes[i] > posDeadzone ||
+				controller.axes[i] < negDeadzone
+			) {
 				console.log(controller.axes[i]);
-                console.log(axis);
-                if(axis.length === 0){
-				    axis.push(controller.axes);
-                }
-                else{
-                    axis.splice(0, 1, controller.axes);   
-                }
+                axis.splice(0, 1, controller.axes);
+                lsR = axis[0][0];
+                lsL = axis[0][0];
+                lsU = axis[0][1];
+                lsD = axis[0][1];
+                rsR = axis[0][2];
+                rsL = axis[0][2];
+                rsU = axis[0][3];
+                rsD = axis[0][3];
+
+				console.log("axis[0][0] " + axis[0][0]);
+				console.log("axis[0][1] " + axis[0][1]);
+				console.log("axis[0][2] " + axis[0][2]);
+				console.log("axis[0][3] " + axis[0][3]);
+                return axis;
 			}
 		}
-}}
+	}
+}
 
 function buttonPressHandler(button) {
 	var press = false;
@@ -69,12 +95,9 @@ function buttonPressHandler(button) {
 
 function axesPressHandler(axis) {
 	var touched = false;
-    console.log('from handlers ' + axis[0])
-	for (var i = 0; i < axis.length; ++i) {
-		if (axis[i] > .8) {
-			touched = true;
-		}
-    }
+	if (axis > posDeadzone || axis < negDeadzone) {
+		touched = true;
+	}
 	return touched;
 }
 
@@ -103,22 +126,67 @@ Options = 8
 
 //do something with the presses
 function pollGamepads() {
+
+    //when button 0 is pressed...
 	if (buttonPressHandler(0)) {
 		console.log(`Button 0 pressed`);
 		updateHandler();
-	}
-
+	} 
+    
     else if (buttonPressHandler(1)) {
 		console.log(`Button 1 pressed`);
 		updateHandler();
-	} 
-  
-    //this needs to be worked on
-    /*
-    else if(axesPressHandler(axis)){
-		//console.log('axis is ' +axis);
+	}
+
+    //when left axis stick moves right...
+    else if(axesPressHandler(lsR > posDeadzone)){
+		console.log('lsR moved ' +axis);
+        lsR = 0;
 		updateHandler();
     }
-    */
-    else updateHandler();
+
+    else if(axesPressHandler(lsL < negDeadzone)){
+		console.log('lsL moved ' +axis);
+        lsL = 0;
+		updateHandler();
+    }
+
+    else if(axesPressHandler(lsU < negDeadzone)){
+		console.log('lsU moved ' +axis);
+        lsU = 0;
+		updateHandler();
+    }
+
+    else if(axesPressHandler(lsD > posDeadzone)){
+		console.log('lsD moved ' +axis);
+        lsD = 0;
+		updateHandler();
+    }
+
+    else if(axesPressHandler(rsR > posDeadzone)){
+		console.log('rsR moved ' +axis);
+        rsR = 0;
+		updateHandler();
+    }
+
+    else if(axesPressHandler(rsL < negDeadzone)){
+		console.log('rsL moved ' +axis);
+        rsL = 0;
+		updateHandler();
+    }
+
+    else if(axesPressHandler(rsU < negDeadzone)){
+		console.log('rsU moved ' +axis);
+        rsU = 0;
+		updateHandler();
+    }
+
+    else if(axesPressHandler(rsD > posDeadzone)){
+		console.log('rsD moved ' +axis);
+        rsD = 0;
+		updateHandler();
+    }
+
+
+	else updateHandler();
 }
