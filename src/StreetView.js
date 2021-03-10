54fd924e3a2747bb6fd1f64a80ca310ef2a17ea0
StevonShaw <Stevonshaw@yahoo.com>
2021-03-10 13:29:11 -0800

    Add functionality to Starting Location button.
    Input is now read from Starting Location input field.
    Input is passed to the Geocoding API, to recieve a lists of locations.
    The first location is used, and lng and lat are pulled from there.
    lng and lat, are then passed to update the panoramic image.

--- src/StreetView.js
+++ src/StreetView.js
@@ -19,7 +19,7 @@ function initMap() {
     zoomControl: false,
   } 
   panorama.setOptions(options);
-  
+
   sv.getPanorama({ location: location, radius: 50 }, processSVData);
 }
 
@@ -41,3 +41,50 @@ function processSVData(data, status) {
   }
 }
 
+function reloadAPIs(key) {
+  // TODO: Dynamically reload, the various Google Map API/services, with the new API Key.
+  // This is non-trival, as on first visit to our site, the index.html page loads the Google Maps API
+  // Therefore, we need a way to either (1) unload the scripts, and reload them with the new key
+  // (2) dynamically modify/adjust what key the app charges to, after the APIs have loaded.
+
+  // Work in progress - Roadblock.
+  alert("Feature is in progress. - for the time, manually set your API Key in the index.html file.")
+  // let scriptElement = document.getElementById("Google-Maps-API-Script");
+  // let newSrcAttribute = 'https://maps.googleapis.com/maps/api/js?key=' + key + '&callback=initMap';
+  // scriptElement.setAttribute("src", newSrcAttribute);
+}
+
+function SetStartingLocation() {
+  // read and clear user input (rough address string)
+  let inputField = document.getElementById("Starting-Location-Input");
+  const inputedAddress = inputField.value;
+  inputField.value = '';
+
+ // TODO: Utilize Places API to ensure the user input is formatted into a proper address string
+
+  // Build GeoCoding request Object
+  const request = {
+    'address': inputedAddress
+  }
+
+  // Build GeoCoder object -- can be moved out to global
+  let geocoder = new google.maps.Geocoder();
+
+  // Make request for location data, with requested area.
+  geocoder.geocode(request, processGeocodeData);
+}
+
+function processGeocodeData(results, status) {
+  if (status === "OK") {
+    // For example, just grabbing the first result from the array of results.
+    const location = results[0].geometry.location;
+    let position = {
+      'lat': location.lat(),
+      'lng': location.lng(),
+    }
+    setNewPano(position)
+
+  } else {
+    console.error("GeoCoding information not found for this location.");
+  }
+}
