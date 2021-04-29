74f2a057ad0da2d2d821d6e87d1618ebcb370e10
ChrisShocker <fallenchris@live.com>
2021-04-28 19:09:00 -0700

    refactor pollgamepads
    added function menu controls
    renamed pollGamepad to pollControls

--- src/gamepad.js
+++ src/gamepad.js
@@ -11,12 +11,12 @@ const deadzone = 0.8;
 let id = 0;
 
 window.addEventListener("gamepadconnected", (e) => {
-		controllerStatus.setAttribute("fill", "green");
-		id = setInterval(pollGamepads, 150);
+	controllerStatus.setAttribute("fill", "green");
+	id = setInterval(pollGamepads, 150);
 });
 
 window.addEventListener("gamepaddisconnected", (event) => {
-		controllerStatus.setAttribute("fill", "red");
+	controllerStatus.setAttribute("fill", "red");
 	clearInterval(id);
 });
 
@@ -61,247 +61,251 @@ let AK = document.getElementById("AK");
 let option;
 let loadedMenu = false;
 let path = getPath();
-let botidx=2001;
-let preidx=2001;
-let hrefidx=0;
-let phrefidx=0;
-let botlen=5;
+let botidx = 2001;
+let preidx = 2001;
+let hrefidx = 0;
+let phrefidx = 0;
+let botlen = 5;
 
 let prebot;
 let curbot;
 let preurl;
 let cururl;
-let subMenulst=document.getElementsByClassName("subMenu");
-let subMenuflag=0;
-let urllist=[];
+let subMenulst = document.getElementsByClassName("subMenu");
+let subMenuflag = 0;
+let urllist = [];
 
-function pollGamepads() {
+function pollControls() {
 	let controller = navigator.getGamepads()[0];
-
-	if (controller == undefined) {
+	if (controller === undefined) {
 		clearInterval(id);
 		return;
 	}
 
-	//Do things if on the index page
 	if (path === "index") {
-		if (loadedMenu === false) {
-			option = MM;
-			option.style.fontSize = "large";
-			//close the accordian menu
-			window.location.href = "#";
-			loadedMenu = true;
-		}
+		menuControls(controller);
+	}
+}
 
-		//Dpad Down
-		if (controller.buttons[13].pressed) {
-			//set flag or Dpadchange function , 0 is go down
-			if(subMenuflag === 1)
-				Urlchange(0);
-			else
-				Dpadchange(0);
-		}
 
-		//Dpad UP
-		else if (controller.buttons[12].pressed) {
-			//set flag or Dpadchange function , 0 is go up
-			if(subMenuflag === 1)
-				Urlchange(1);
+function menuControls(controller) {
+	if (loadedMenu === false) {
+		option = MM;
+		option.style.fontSize = "large";
+		//close the accordian menu
+		window.location.href = "#";
+		loadedMenu = true;
+	}
+
+	//Dpad Down
+	if (controller.buttons[13].pressed) {
+		//set flag or Dpadchange function , 0 is go down
+		if (subMenuflag === 1)
+			Urlchange(0);
+		else
+			Dpadchange(0);
+	}
+
+	//Dpad UP
+	else if (controller.buttons[12].pressed) {
+		//set flag or Dpadchange function , 0 is go up
+		if (subMenuflag === 1)
+			Urlchange(1);
+		else
+			Dpadchange(1);
+	}
+
+	else if (controller.buttons[0].pressed) {
+		var subMenuidx;
+		subMenuidx = (Math.abs(botidx) - 1) % 5;
+		if (subMenuflag === 1) {
+			if (Math.abs(hrefidx) % urllist.length == urllist.length - 1)
+				Backtomenu();
 			else
-				Dpadchange(1);
+				window.location.href = urllist[Math.abs(hrefidx) % urllist.length].href;
 		}
-		
-		else if(controller.buttons[0].pressed){
-			var subMenuidx;
-			subMenuidx=(Math.abs(botidx)-1)%5;			
-			if(subMenuflag === 1){
-				if(Math.abs(hrefidx)%urllist.length==urllist.length-1)
-					Backtomenu();
-				else
-					window.location.href=urllist[Math.abs(hrefidx)%urllist.length].href;
-			}
-			else
-			if(subMenuflag === 0){
-				for(var i=0;i<subMenulst[subMenuidx].childNodes.length;i++){
-					if(subMenulst[subMenuidx].childNodes[i].nodeType==1){
+		else
+			if (subMenuflag === 0) {
+				for (var i = 0; i < subMenulst[subMenuidx].childNodes.length; i++) {
+					if (subMenulst[subMenuidx].childNodes[i].nodeType == 1) {
 						urllist.push(subMenulst[subMenuidx].childNodes[i]);
 					}
 				}
-				subMenuflag=1;
-				cururl=urllist[0];
+				subMenuflag = 1;
+				cururl = urllist[0];
 				cururl.style.fontSize = "large";
-				window.location.href=curbot.href;
-				hrefidx+=urllist.length*200;
-				phrefidx+=urllist.length*200;	
+				window.location.href = curbot.href;
+				hrefidx += urllist.length * 200;
+				phrefidx += urllist.length * 200;
 			}
-		}
+	}
 
-		else if (controller.buttons[1].pressed) {
-			Backtomenu();
-		}
-		
-		//AXES
-		const leftStickXAxis = Math.abs(controller.axes[0]);
-		const leftStickYAxis = Math.abs(controller.axes[1]);
-		const rightStickXAxis = Math.abs(controller.axes[2]);
-		const rightStickYAxis = Math.abs(controller.axes[3]);
-		if (leftStickXAxis > deadzone) {
-			console.log(controller.axes[0]);
-		}
-		if (leftStickYAxis > deadzone) {
-			console.log(controller.axes[1]);
-		}
-		if (rightStickXAxis > deadzone) {
-			console.log(controller.axes[2]);
-		}
-		if (rightStickYAxis > deadzone) {
-			console.log(controller.axes[3]);
+	else if (controller.buttons[1].pressed) {
+		Backtomenu();
+	}
+
+	//AXES
+	const leftStickXAxis = Math.abs(controller.axes[0]);
+	const leftStickYAxis = Math.abs(controller.axes[1]);
+	const rightStickXAxis = Math.abs(controller.axes[2]);
+	const rightStickYAxis = Math.abs(controller.axes[3]);
+	if (leftStickXAxis > deadzone) {
+		console.log(controller.axes[0]);
+	}
+	if (leftStickYAxis > deadzone) {
+		console.log(controller.axes[1]);
+	}
+	if (rightStickXAxis > deadzone) {
+		console.log(controller.axes[2]);
+	}
+	if (rightStickYAxis > deadzone) {
+		console.log(controller.axes[3]);
+	}
+}
+
+//If not in menu path, press 'b' to goback to menu page
+if (path != "index") {
+	if (controller.buttons[1].pressed) {
+		window.location.href = "index.html";
+	}
+}
+
+//Do things if on the drive page
+if (path === "drive") {
+	//BUTTONS
+	if (controller.buttons[0].pressed) {
+		console.log(`Button 0 pressed`);
+	}
+
+	if (controller.buttons[1].pressed) {
+		console.log(`Button 1 pressed`);
+	}
+
+	if (controller.buttons[8].pressed || controller.buttons[9].pressed) {
+		let xboxScheme = document.getElementById("picture");
+		if (isSchemeOn === true) {
+			let image = document.getElementById("controller-scheme");
+			xboxScheme.removeChild(image);
+			isSchemeOn = false;
+		} else {
+			let image = new Image();
+			image.src = "../assets/svDriveGamepad.png";
+			image.id = "controller-scheme";
+			xboxScheme.appendChild(image);
+
+			isSchemeOn = true;
 		}
 	}
 
-	//If not in menu path, press 'b' to goback to menu page
-	if(path!="index"){
-		if (controller.buttons[1].pressed) {
-			window.location.href="index.html";
+	//AXES
+	const leftStickXAxis = Math.abs(controller.axes[0]);
+	const leftStickYAxis = Math.abs(controller.axes[1]);
+	const rightStickXAxis = Math.abs(controller.axes[2]);
+	const rightStickYAxis = Math.abs(controller.axes[3]);
+	if (leftStickXAxis > deadzone) {
+		let heading;
+		let links = _panorama.getLinks();
+
+		if (controller.axes[1] > 0) {
+			heading = _display.vehicleHeading + 90;
+			if (heading > 360) heading = heading - 360;
 		}
+		else {
+			heading = _display.vehicleHeading - 90;
+			if (heading <= 0) heading = 360 + heading;
+		}
+
+		let minDifferenceIndex;
+		let minDifference = 360;
+		for (let i = 0; i < links.length; ++i) {
+			let leftDiff = Math.abs(heading - links[i].heading);
+			let rightDiff = 360 - leftDiff;
+			let diff = Math.min(leftDiff, rightDiff);
+			if (diff < minDifference && diff < 45) {
+				minDifference = diff;
+				minDifferenceIndex = i;
+			}
+		}
+		if (minDifferenceIndex === undefined) return;
+
+		if (controller.axes[1] > 0)
+			_display.heading += minDifference;
+		else
+			_display.heading -= minDifference;
+
+		_display.vehicleHeading = links[minDifferenceIndex].heading;
+		_display.processSVData({ location: { pano: `${links[minDifferenceIndex].pano}`, } }, "OK");
 	}
+	if (leftStickYAxis > deadzone) {
+		let heading;
+		let links = _panorama.getLinks();
+		if (controller.axes[1] > 0) heading = _display.vehicleHeading - 180;
+		else heading = _display.vehicleHeading;
 
-	//Do things if on the drive page
-	if (path === "drive") {
-		//BUTTONS
-		if (controller.buttons[0].pressed) {
-			console.log(`Button 0 pressed`);
-		}
-
-		if (controller.buttons[1].pressed) {
-			console.log(`Button 1 pressed`);
-		}
-
-		if (controller.buttons[8].pressed || controller.buttons[9].pressed) {
-			let xboxScheme = document.getElementById("picture");
-			if (isSchemeOn === true) {
-				let image = document.getElementById("controller-scheme");
-				xboxScheme.removeChild(image);
-				isSchemeOn = false;
-			} else {
-				let image = new Image();
-				image.src = "../assets/svDriveGamepad.png";
-				image.id = "controller-scheme";
-				xboxScheme.appendChild(image);
-
-				isSchemeOn = true;
+		let minDifferenceIndex;
+		let minDifference = 360;
+		for (let i = 0; i < links.length; ++i) {
+			let leftDiff = Math.abs(heading - links[i].heading);
+			let rightDiff = 360 - leftDiff;
+			let diff = Math.min(leftDiff, rightDiff);
+			if (diff < minDifference && diff < 45) {
+				minDifference = diff;
+				minDifferenceIndex = i;
 			}
 		}
 
-		//AXES
-		const leftStickXAxis = Math.abs(controller.axes[0]);
-		const leftStickYAxis = Math.abs(controller.axes[1]);
-		const rightStickXAxis = Math.abs(controller.axes[2]);
-		const rightStickYAxis = Math.abs(controller.axes[3]);
-		if (leftStickXAxis > deadzone) {
-			let heading;
-			let links = _panorama.getLinks();
-
-			if (controller.axes[1] > 0) {
-				heading = _display.vehicleHeading + 90;
-				if (heading > 360) heading = heading - 360;
-			}
-			else {
-				heading = _display.vehicleHeading - 90;
-				if (heading <= 0) heading = 360 + heading;
-			}
-
-			let minDifferenceIndex;
-			let minDifference = 360;
-			for (let i = 0; i < links.length; ++i){
-				let leftDiff = Math.abs(heading - links[i].heading);
-				let rightDiff = 360 - leftDiff;
-				let diff = Math.min(leftDiff, rightDiff);
-				if (diff < minDifference && diff < 45) {
-					minDifference = diff;
-					minDifferenceIndex = i;
-				}
-			}
-			if (minDifferenceIndex === undefined) return;
-			
-			if (controller.axes[1] > 0)
-				_display.heading += minDifference;
-			else
-				_display.heading -= minDifference;
-			
-			_display.vehicleHeading = links[minDifferenceIndex].heading;
-			_display.processSVData({ location: { pano: `${links[minDifferenceIndex].pano}`, } }, "OK");
-		}
-		if (leftStickYAxis > deadzone) {
-			let heading;
-			let links = _panorama.getLinks();
-			if (controller.axes[1] > 0) heading = _display.vehicleHeading - 180;
-			else heading = _display.vehicleHeading;
-			
-			let minDifferenceIndex;
-			let minDifference = 360;
-			for (let i = 0; i < links.length; ++i){
-				let leftDiff = Math.abs(heading - links[i].heading);
-				let rightDiff = 360 - leftDiff;
-				let diff = Math.min(leftDiff, rightDiff);
-				if (diff < minDifference && diff < 45) {
-					minDifference = diff;
-					minDifferenceIndex = i;
-				}
-			}
-
-			if (minDifferenceIndex === undefined) return;
-			_display.processSVData({ location: { pano: `${links[minDifferenceIndex].pano}`, } }, "OK");
-		}
-		if (rightStickXAxis > deadzone) {
-			_display.heading += controller.axes[2] * 10;
-			_panorama.setPov({ heading: _display.heading, pitch: _display.pitch })
-		}
-		if (rightStickYAxis > deadzone) {
-			let pitch = _display.pitch - controller.axes[3] * 10;
-			if (pitch > 90) {
-				pitch = 90
-			}
-			if (pitch < -90) {
-				pitch = -90
-			}
-			_display.pitch = pitch;
-			_panorama.setPov({ heading: _display.heading, pitch: _display.pitch })
-		}
+		if (minDifferenceIndex === undefined) return;
+		_display.processSVData({ location: { pano: `${links[minDifferenceIndex].pano}`, } }, "OK");
 	}
+	if (rightStickXAxis > deadzone) {
+		_display.heading += controller.axes[2] * 10;
+		_panorama.setPov({ heading: _display.heading, pitch: _display.pitch })
+	}
+	if (rightStickYAxis > deadzone) {
+		let pitch = _display.pitch - controller.axes[3] * 10;
+		if (pitch > 90) {
+			pitch = 90
+		}
+		if (pitch < -90) {
+			pitch = -90
+		}
+		_display.pitch = pitch;
+		_panorama.setPov({ heading: _display.heading, pitch: _display.pitch })
+	}
+}
 } // End pollGamepads
 
-function Dpadchange(flag){
+function Dpadchange(flag) {
 	//preidx: indx of prenode 
-	preidx=botidx;
-	if(flag==1)
-		botidx-=1;
+	preidx = botidx;
+	if (flag == 1)
+		botidx -= 1;
 	else
-		botidx+=1;
-	prebot=document.getElementById((Math.abs(preidx)%botlen).toString());
-	curbot=document.getElementById((Math.abs(botidx)%botlen).toString());
-	prebot.style.fontSize ="medium";
+		botidx += 1;
+	prebot = document.getElementById((Math.abs(preidx) % botlen).toString());
+	curbot = document.getElementById((Math.abs(botidx) % botlen).toString());
+	prebot.style.fontSize = "medium";
 	curbot.style.fontSize = "large";
 }
 
-function Urlchange(flag){
+function Urlchange(flag) {
 	//preidx: indx of prenode 
-	phrefidx=hrefidx;
-	if(flag==1)
-		hrefidx-=1;
+	phrefidx = hrefidx;
+	if (flag == 1)
+		hrefidx -= 1;
 	else
-		hrefidx+=1;
-	preurl=urllist[Math.abs(phrefidx)%urllist.length];
-	cururl=urllist[Math.abs(hrefidx)%urllist.length];
-	preurl.style.fontSize ="medium";
+		hrefidx += 1;
+	preurl = urllist[Math.abs(phrefidx) % urllist.length];
+	cururl = urllist[Math.abs(hrefidx) % urllist.length];
+	preurl.style.fontSize = "medium";
 	cururl.style.fontSize = "large";
 }
 
-function Backtomenu(){
-	urllist.length=0;
-	subMenuflag=0;
-	window.location.href="#";
+function Backtomenu() {
+	urllist.length = 0;
+	subMenuflag = 0;
+	window.location.href = "#";
 	cururl.style.fontSize = "medium";
-	hrefidx=0;
-	phrefidx=0;	
+	hrefidx = 0;
+	phrefidx = 0;
 }
