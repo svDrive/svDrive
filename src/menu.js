96a60c3ad099b102a16323dde8b9bb32b528a4b0
Micah Blasko <mblasko@pdx.edu>
2021-05-14 23:07:30 -0700

    Added logic for modal popups and required api

--- src/menu.js
+++ src/menu.js
@@ -1,27 +1,88 @@
 // Sets a sanitized copy of the input string as the current API-Key.
 function setAPIKey(unsanitizedInput) {
-    let sanitizedInput = sanitizeGMapKey(unsanitizedInput)
-    localStorage.setItem("API-KEY", sanitizedInput);
+  let sanitizedInput = sanitizeGMapKey(unsanitizedInput);
+  localStorage.setItem("API-KEY", sanitizedInput);
 }
 
 // Removes all charcaters which are invalid for a Google Maps API Key from the input string.
 // Aggressively eleminates XXS attempts, as nearly every special character is removed.
 function sanitizeGMapKey(unsanitizedInput) {
-    let cleanInput = String()
-    let testChar = ''
-    for (var pos = 0; pos < unsanitizedInput.length; pos++){
-        testChar = unsanitizedInput.charAt(pos)
-        if (_validCharacterSet.includes(testChar))
-            cleanInput += testChar
-    }
-    return cleanInput
+  let cleanInput = String();
+  let testChar = "";
+  for (var pos = 0; pos < unsanitizedInput.length; pos++) {
+    testChar = unsanitizedInput.charAt(pos);
+    if (_validCharacterSet.includes(testChar)) cleanInput += testChar;
+  }
+  return cleanInput;
 }
 
+function drive() {
+  window.location.replace("drive.html");
+}
 
-const apiInput = document.getElementById("API-Key");
-apiInput.addEventListener("keyup", function (event) {
-    if (event.key === "Enter") {
-        setAPIKey(apiInput.value)
-        apiInput.value = ""
+/*****************************
+Welcome configuration modal
+*****************************/
+window.addEventListener("load", function () {
+  //Keeps modal from closing by clicking outside it
+  $("#welcomeModal").modal({
+    backdrop: "static",
+    keyboard: false,
+  });
+  if (
+    localStorage.getItem("API-KEY") == undefined ||
+    localStorage.getItem("CONTROLLER-TYPE") == undefined
+  ) {
+    $("#welcomeModal").modal("show");
+  }
+});
+
+const welcomeApiInput = document.querySelector("#API-Key1");
+const welcomeControllerInput = document.getElementsByName("controllerType1");
+const welcomeSetButton = document.querySelector("#welcomeSet");
+welcomeSetButton.addEventListener("click", function () {
+  if (welcomeApiInput.value != "") {
+    setAPIKey(welcomeApiInput.value);
+  } else {
+    window.alert("Must enter an API Key!");
+    return;
+  }
+  for (let i = 0; i < welcomeControllerInput.length; i++) {
+    if (welcomeControllerInput[i].checked) {
+      localStorage.setItem("CONTROLLER-TYPE", welcomeControllerInput[i].value);
     }
+  }
+  $("#welcomeModal").modal("hide");
+});
+
+/*****************************
+API KEY configuration modal
+*****************************/
+
+const apiInput = document.getElementById("API-Key2");
+const apiSetButton = document.querySelector("#apiSet");
+apiSetButton.addEventListener("click", function (event) {
+  if (apiInput.value != "") {
+    setAPIKey(apiInput.value);
+    apiInput.value = "";
+    $("#apiKeyModal").modal("hide");
+  } else {
+    window.alert("API Key input is blank");
+  }
+});
+
+/*****************************
+Controller configuration modal
+*****************************/
+
+const controllerInput = document.getElementsByName("controllerType2");
+const controllerSetButton = document.querySelector("#controllerSet");
+
+controllerSetButton.addEventListener("click", function () {
+  for (let i = 0; i < controllerInput.length; i++) {
+    if (controllerInput[i].checked) {
+      localStorage.setItem("CONTROLLER-TYPE", controllerInput[i].value);
+    }
+  }
+  $("#controllerModal").modal("hide");
 });
